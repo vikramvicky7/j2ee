a6c909a9c22828bcff185942f024025afeffeb1e
vikramvicky7 <49385106+vikramvicky7@users.noreply.github.com>
2019-04-08 11:02:40 +0530

    Add files via upload

---
+++ src/filter/MyFilter.java
@@ -0,0 +1,46 @@
+package filter;
+
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class MyFilter implements Filter {
+
+	public void init(FilterConfig arg0) throws ServletException {
+		System.out.println("init");
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpSession session = ((HttpServletRequest) req).getSession();
+
+		String user = req.getParameter("name");
+		String password = req.getParameter("password");
+
+		if (user != null && password != null && user.equals("vikram") && password.equals("vicky123")) {
+			session.setAttribute("user", user);
+			System.out.println("Name in session=" + session.getAttribute("user"));
+			chain.doFilter(req, resp);
+		}
+
+		else {
+			RequestDispatcher rd = req.getRequestDispatcher("index.html");
+			rd.include(req, resp);
+		}	
+	}
+
+	public void destroy() {
+		
+	}
+
+}
